2bd3ed1d7a29dd54503120e759b6ba80995a47d7
Yurii Rashkovskii <yrashk@gmail.com>
2012-10-12 16:16:42 -0700

    Make .app contain keywords required by systools
    (Until mix starts populating them by default)

--- mix.exs
+++ mix.exs
@@ -9,11 +9,9 @@ defmodule Relex.Mixfile do
 
   # Configuration for the OTP application
   def application do
-    []
+    [description: 'Hello', registered: []]
   end
 
-  # Returns the list of dependencies in the format:
-  # { :foobar, "0.1", git: "https://github.com/elixir-lang/foobar.git" }
   defp deps do
     []
   end
