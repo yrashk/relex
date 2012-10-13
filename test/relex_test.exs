ba371a21c5d2cbf96f9882661d9d5f4a67a351b3
Yurii Rashkovskii <yrashk@gmail.com>
2012-10-13 09:04:05 -0700

    Add a very simple sanity test (that TestRelease can compile)

--- test/relex_test.exs
+++ test/relex_test.exs
@@ -1,9 +1,16 @@
 Code.require_file "../test_helper.exs", __FILE__
 
+defmodule TestRelease do
+  use Relex.Release
+
+  def name, do: "test_rel"
+  def version, do: "1.0"
+end
+
 defmodule RelexTest do
   use ExUnit.Case
 
-  test "the truth" do
-    assert true
+  test "basic module sanity test" do
+    assert Code.ensure_loaded?(TestRelease) == true
   end
 end
