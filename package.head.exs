f18651dea13d4a991bb0382d24c4611b49240cff
Yurii Rashkovskii <yrashk@gmail.com>
2012-10-12 17:21:09 -0700

    Add expm package

---
+++ package.head.exs
@@ -0,0 +1,6 @@
+Expm.Package.new(name: "relex", description: "Release generator",
+                 version: :head, keywords: ["release","systools","boot","script","reltool"], 
+                 maintainers: [[name: "Yurii Rashkovskii", 
+                                email: "yrashk@gmail.com"]],
+                 repositories: [[github: "yrashk/relex"]])
+    
