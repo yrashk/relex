88e355c354fd702d6468aa2133f5402c5fb16233
Yurii Rashkovskii <yrashk@gmail.com>
2012-10-14 07:54:35 -0700

    Add more tests

--- test/relex_test.exs
+++ test/relex_test.exs
@@ -10,7 +10,22 @@ end
 defmodule RelexTest do
   use ExUnit.Case
 
+  def setup_all do
+    File.mkdir_p!(path)
+    path
+  end
+
+  def teardown_all(path) do
+    File.rm_rf! path
+  end
+
   test "basic module sanity test" do
     assert Code.ensure_loaded?(TestRelease) == true
   end
+
+  test "assembly of a minimal release" do
+    assert TestRelease.assemble!(path: path) == :ok
+  end
+
+  defp path, do: File.join(File.dirname(__FILE__), "tmp")
 end
